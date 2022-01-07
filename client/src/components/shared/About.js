e65f25e4b32fdddf7c930874da569c64edf9f339
Chantel Cooke <chantelmcooke@gmail.com>
2022-01-06 17:48:53 -0700

    about and contact updated

--- client/src/components/shared/About.js
+++ client/src/components/shared/About.js
@@ -3,7 +3,7 @@ const About = () => {
     <>
     <h1>About Page</h1>
       <h4>About Me</h4>
-      <p>Something about me.
+      <p>I find joy in many things: my family, the outdoors, sweet corn (Iowa raised), and developing my skills in creativity, problem solving, communication, and leadership. My experiences as a graphic designer, photographer, clothing designer, and office administrator have helped me develop a well-rounded skill set to understand how to communicate with clients and team members for successful professional relationships, work smoothly through problems, think outside the box, and manage my time both as a professional and as a mother of two crazy kids.
       </p>
     </>
   )
